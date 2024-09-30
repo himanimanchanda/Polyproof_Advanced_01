2e46d3879af5f8cb9b370e2274eb0c2f1e91fb4c
Himani Manchanda <142446743+himanimanchanda@users.noreply.github.com>
2024-09-30 19:19:47 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "this is my favourite music",
+    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+  },
+  {
+    name: "1",
+    description: "this is my favourite music",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, //change
+  {
+    name: "2",
+    description: "this is my favourite music",
+    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+  },
+  {
+    name: "3",
+    description: "this is my favourite music",
+    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+  },
+  {
+    name: "4",
+    description: "this is my favourite music.",
+    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+  },
+];
+
+module.exports = nfts;
